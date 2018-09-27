536e8a51b4dd030d849699eb9bf3d3440ef1f412
javakarthik <32713545+javakarthik@users.noreply.github.com>
2018-09-27 22:15:07 +0530

    Updated

--- account-service/src/main/java/com/piggymetrics/account/controller/AccountController.java
+++ account-service/src/main/java/com/piggymetrics/account/controller/AccountController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class AccountController {
+	private static final Logger LOG = LoggerFactory.getLogger(AccountController.class.getName());
+
 
 	@Autowired
 	private AccountService accountService;
@@ -19,21 +24,25 @@ public class AccountController {
 	@PreAuthorize("#oauth2.hasScope('server') or #name.equals('demo')")
 	@RequestMapping(path = "/{name}", method = RequestMethod.GET)
 	public Account getAccountByName(@PathVariable String name) {
+		LOG.info("getAccountByName");
 		return accountService.findByName(name);
 	}
 
 	@RequestMapping(path = "/current", method = RequestMethod.GET)
 	public Account getCurrentAccount(Principal principal) {
+		LOG.info("getCurrentAccount");
 		return accountService.findByName(principal.getName());
 	}
 
 	@RequestMapping(path = "/current", method = RequestMethod.PUT)
 	public void saveCurrentAccount(Principal principal, @Valid @RequestBody Account account) {
+		LOG.info("saveCurrentAccount");
 		accountService.saveChanges(principal.getName(), account);
 	}
 
 	@RequestMapping(path = "/", method = RequestMethod.POST)
 	public Account createNewAccount(@Valid @RequestBody User user) {
+		LOG.info("createNewAccount");
 		return accountService.create(user);
 	}
 }
